a57dcc4bd94058bb67342e9e280ae864f4695a88
Erfanmah7 <efn8mah@yahoo.com>
2024-05-13 04:37:02 +0330

    updating life cycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
 import User from "./User";
 
 function App() {
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      const json = await res.json();
-      setUser(json);
-    };
-
-    fetchData();
-  
-  }, []);
-
   return (
     <>
-      <User user={user}/>
+      <User />
     </>
   );
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-function User({ user }) {
+function User() {
+  const [user, setUser] = useState([]);
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      const json = await res.json();
+      setUser(json);
+      console.log(json);
+    };
+    fetchData();
+  }, [id]);
+
   return (
     <div>
-      <ul>
+      {!user.length && <p>Loading</p>}
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      {/* <ul>
         {user.map((u) => (
           <li key={u.id}>{u.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
