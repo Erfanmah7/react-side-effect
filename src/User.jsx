6dd67612b0e74999ed0d86bb2f1232d18e67d68a
Erfanmah7 <efn8mah@yahoo.com>
2024-05-14 23:45:24 +0330

    handel fetch

--- src/User.jsx
+++ src/User.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import Counter from "./Counter";
 
-function User({ user }) {
+function User() {
+  const [user, setUser] = useState([]);
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const json = await res.json();
+        setUser(json);
+      } catch (errorr) {
+        console.log(errorr);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const clickHandler = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const json = await res.json();
+    console.log(json);
+  };
+
   return (
     <div>
+      {id > 10 && <Counter />}
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={clickHandler}>search</button>
       <ul>
         {user.map((u) => (
           <li key={u.id}>{u.name}</li>
